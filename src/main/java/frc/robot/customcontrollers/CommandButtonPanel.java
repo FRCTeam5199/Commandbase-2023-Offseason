0ad24dbe6231ebba333998822064a8192d426245
Nikki Chong <chong.jeffrie@gmail.com>
2023-08-14 20:09:19 -0700

    CommandButtonPanel expansion
    - added (nearly) infinite expansion for how many Arduinos we can use
    - keeps a CommandGenericHID open for each port, and references it (instead of making a new one), keeping memory as low as possible
    - likely compatible with former code references (if not, simple fixes TODO)

--- src/main/java/frc/robot/customcontrollers/CommandButtonPanel.java
+++ src/main/java/frc/robot/customcontrollers/CommandButtonPanel.java
@@ -1,23 +1,39 @@
 package frc.robot.customcontrollers;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+import java.util.List;
+
+// CommandButtonPanel takes in many CommandGenericHIDs (what we use for the Arduinos in the button panels),
+// and stores them in a List<> in order to reference them later on
+
 public class CommandButtonPanel {
+    // Each Arduino uses 1 port and can have 16 buttons
+    List<CommandGenericHID> ports;
+    List<Integer> portIDs;
 
-    CommandGenericHID buttonPanel1;
-    CommandGenericHID buttonPanel2;
+    // Varargs constructor that initializes new ports and stores their port IDs
+    public CommandButtonPanel(List<Integer> IDs) { 
+        portIDs = IDs;
 
-    public CommandButtonPanel(int port1, int port2) {
-        buttonPanel1 = new CommandGenericHID(port1);
-        buttonPanel2 = new CommandGenericHID(port2);
+        for (int portID : IDs) { 
+            ports.add(new CommandGenericHID(portID)); // Make new port for each portID given
+        }
     }
 
-    public Trigger button(int port, int id) {
-        return port == 2 ? buttonPanel1.button(id) : buttonPanel2.button(id);
-    }
+    // References .button() Trigger of a port, accounting for if it already exists
+    public Trigger button(int portID, int buttonID){
 
+        int portIDIndex = portIDs.indexOf(portID); // Should be >= 0 if a port with given portID already exists in ports
+
+        if (portIDIndex >= 0) {
+            return ports.get(portIDIndex).button(buttonID); // References existing port's .button() Trigger
+        } else {
+            portIDs.add(portID); // If port doesn't exist yet, register new port in ports<>
+            ports.add(new CommandGenericHID(portID));
+
+            return ports.get(-1).button(buttonID); // Reference the .button() Trigger of the latest item added at end of ports<> list
+        }
+    }
 }
