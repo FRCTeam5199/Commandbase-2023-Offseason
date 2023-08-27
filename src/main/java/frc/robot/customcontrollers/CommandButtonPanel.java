5b605983b2faace7502fd25f84a31584f0697e1e
Nikki Chong <chong.jeffrie@gmail.com>
2023-08-26 21:33:39 -0700

    this
    added "this"

--- src/main/java/frc/robot/customcontrollers/CommandButtonPanel.java
+++ src/main/java/frc/robot/customcontrollers/CommandButtonPanel.java
@@ -20,23 +20,23 @@ public class CommandButtonPanel {
         ports = new ArrayList<CommandGenericHID>(IDs.length);
 
         for (int portID : IDs) { 
-            ports.add(new CommandGenericHID(portID)); // Make new port for each portID given
-            portIDs.add(portID);
+            this.ports.add(new CommandGenericHID(portID)); // Make new port for each portID given
+            this.portIDs.add(portID);
         }
     }
 
     // References .button() Trigger of a port, accounting for if it already exists
     public Trigger button(int portID, int buttonID){
 
-        int portIDIndex = portIDs.indexOf(portID); // Should be >= 0 if a port with given portID already exists in ports<>
+        int portIDIndex = this.portIDs.indexOf(portID); // Should be >= 0 if a port with given portID already exists in ports<>
 
         if (portIDIndex >= 0) {
-            return ports.get(portIDIndex).button(buttonID); // References existing port's .button() Trigger
+            return this.ports.get(portIDIndex).button(buttonID); // References existing port's .button() Trigger
         } else {
-            portIDs.add(portID); // If port doesn't exist yet, register new port in ports<>
-            ports.add(new CommandGenericHID(portID));
+            this.portIDs.add(portID); // If port doesn't exist yet, register new port in ports<>
+            this.ports.add(new CommandGenericHID(portID));
 
-            return ports.get(ports.size() - 1).button(buttonID); // Reference the .button() Trigger of the latest item added at end of ports<> list
+            return this.ports.get(this.ports.size() - 1).button(buttonID); // Reference the .button() Trigger of the latest item added at end of ports<> list
         }
     }
 }
