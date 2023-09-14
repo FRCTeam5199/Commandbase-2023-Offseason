3d13aafb27e365c1ed8f8aba05551bfb16478c1c
oongaboongadoonga <jreidel8@gmail.com>
2023-09-13 20:05:27 -0700

    Update Constants.java

--- src/main/java/com/frc/robot/Constants.java
+++ src/main/java/com/frc/robot/Constants.java
@@ -64,7 +64,7 @@ public final class Constants {
   public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
   public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
   public static final int BACK_LEFT_MODULE_STEER_ENCODER = 14;
-  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(94);
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(274);
 
   public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 5;
   public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 6;
