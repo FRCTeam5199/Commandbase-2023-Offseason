27cca35bf56261e9912624b3bad48122ab9c9959
oongaboongadoonga <jreidel8@gmail.com>
2023-10-28 18:16:02 -0700

    stuff

--- src/main/java/com/frc/robot/commands/Auton.java
+++ src/main/java/com/frc/robot/commands/Auton.java
@@ -126,7 +126,7 @@ public class Auton {
         true,
         drivetrain // The drive subsystem. Used to properly set the requirements of path following commands
     );
-    autoBuilderRed = new SwerveAutoBuilder(() -> drivetrain.getOdometryPose2dAprilTags(), // Pose2d supplier TODO: possibly revert back to no apriltags
+    autoBuilder3 = new SwerveAutoBuilder(() -> drivetrain.getOdometryPose2dAprilTags(), // Pose2d supplier TODO: possibly revert back to no apriltags
     (pose) -> drivetrain.resetOdometry(pose), // Pose2d consumer, used to reset odometry at the beginning of auto
     Constants.m_kinematics, // SwerveDriveKinematics
     new PIDConstants(1.5, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
