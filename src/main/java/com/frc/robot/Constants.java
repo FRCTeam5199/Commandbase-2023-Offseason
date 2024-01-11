c5da2c76174c7a535f3d2682bb146c9b88252558
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2024-01-10 20:41:20 -0800

    changed front right steer offset

--- src/main/java/com/frc/robot/Constants.java
+++ src/main/java/com/frc/robot/Constants.java
@@ -59,7 +59,7 @@ public final class Constants {
   public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
   public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
   public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12;
-  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(304);
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(214); //Original - 304
 
   public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
   public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
